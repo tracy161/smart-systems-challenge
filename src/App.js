3f87add8507a86b0585f157de15057991b3a6bda
Tram Pham <tracy@users-MacBook-Pro.local>
2022-03-21 01:03:11 +1100

    add loading

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 
 function App() {
   const [state, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const url = process.env.REACT_APP_URL;
@@ -15,6 +16,7 @@ function App() {
         const response = await fetch(url);
         const data = await response.json();
         setData(data);
+        setIsLoading(false);
       } catch (error) {
         console.log('error', error);
       }
@@ -28,21 +30,36 @@ function App() {
       <div className='col-sm-12'>
         <div className='panel panel-dark-outline tabs-panel'>
           <div className='panel-body tab-content'>
-            {state.map((item, index) => (
-              <Panel key={index} item={item} />
-            ))}
+            {!isLoading && state.length === 0 && (
+              <h1 className='text-center mx-auto mt-32'>
+                No Files & Folders Found
+              </h1>
+            )}
+            {isLoading ? (
+              <h1 className='text-center mx-auto mt-32'>Loading...</h1>
+            ) : (
+              <>
+                {state.map((item, index) => (
+                  <Panel key={index} item={item} />
+                ))}
+              </>
+            )}
           </div>
         </div>
       </div>
       <div className='col-sm-12'>
         <div className='panel panel-dark-outline tabs-panel'>
           <div className='panel-body tab-content'>
-            <h1>
-              Total Files: <TotalFiles data={state} />
-            </h1>
-            <h1>
-              Total Size: <TotalSize data={state} />
-            </h1>
+            {!isLoading && (
+              <>
+                <h1>
+                  Total Files: <TotalFiles data={state} />
+                </h1>
+                <h1>
+                  Total Size: <TotalSize data={state} />
+                </h1>
+              </>
+            )}
           </div>
         </div>
       </div>
